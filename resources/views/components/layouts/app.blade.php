fc33c55309337bb3ba4c92440bc0d833acf7999a
jnthnlnnrt <jonathan.lennart@gmail.com>
2024-06-18 00:48:44 -0600

    Asset Manager

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -43,7 +43,7 @@
                 <!-- User menu -->
                 <button type="button" class="flex mx-3 md:mr-0" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="dropdown">
                     <span class="sr-only">Open user menu</span>
-                    <div class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                    <div class="relative inline-flex items-center justify-center w-10 h-10 ring-1 ring-gray-200 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
                         <span class="font-medium text-gray-600 dark:text-gray-300">JL</span>
                     </div>
                     <span class="text-sm self-center ml-3">Jonathan Lennart Cruz Garcia</span>
